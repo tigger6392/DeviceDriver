7fe1d972f390f9d2c34a8d51992071279043ad2f
tw2013.kim <tigger6392@gmail.com>
2023-06-05 13:58:21 +0900

    First setting for VS

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../Project1/DeviceDriver.h"
+#include "../Project1/FlashMemoryDevice.h"
+#include "../Project1/DeviceDriver.cpp"
+
+using namespace testing;
+
+TEST(TestCaseName, TestName) {
+    EXPECT_THAT(1, Eq(1));
+}
